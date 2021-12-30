f3506551ce817c2cef4fc2f45716bd71fb21848c
Alex Marshall <alex.k.marshall83@gmail.com>
2021-12-29 21:53:43 -0800

    add accessibility test for <Avatar>

--- src/components/avatar/avatar.tsx
+++ src/components/avatar/avatar.tsx
@@ -1,35 +1,21 @@
 import * as styles from './avatar.css'
 
-import { useEffect, useState } from 'react'
-
 import NextImage from 'next/image'
 import { assignInlineVars } from '@vanilla-extract/dynamic'
 
 type Props = { username: string; src?: string }
 export function Avatar({ src, username }: Props): JSX.Element {
-  const [backgroundColor, setBackgroundColor] = useState('hsl(0deg 0% 50%)')
-
-  useEffect(() => {
-    getColor(username).then((color) => {
-      setBackgroundColor(color)
-    })
-  }, [username])
-
   const initial = username.charAt(0).toUpperCase()
+  const backgroundColor = getColor(username)
 
   return (
-    <>
-      <span
-        style={assignInlineVars({ [styles.backgroundColor]: backgroundColor })}
-        className={styles.avatar}
-        aria-hidden
-      >
-        {initial}
-        {src ? (
-          <NextImage src={src} alt="" layout="fill" width={48} height={48} />
-        ) : null}
-      </span>
-    </>
+    <span
+      style={assignInlineVars({ [styles.backgroundColor]: backgroundColor })}
+      className={styles.avatar}
+    >
+      <span aria-hidden>{initial}</span>
+      {src ? <NextImage src={src} alt="" layout="fill" /> : null}
+    </span>
   )
 }
 
@@ -39,7 +25,7 @@ type GetColorOptions = {
   minSaturation?: number
   maxSaturation?: number
 }
-async function getColor(
+function getColor(
   text: string,
   {
     minLightness = 60,
@@ -48,18 +34,23 @@ async function getColor(
     maxSaturation = 60,
   }: GetColorOptions = {}
 ) {
-  const hash = await crypto.subtle.digest(
-    'SHA-1',
-    new TextEncoder().encode(text)
-  )
-  const hashString = new Uint8Array(hash).join('').slice(16)
-  const randomNumber = parseInt(hashString)
+  const hashedText = hash(text)
 
-  const randomHue = randomNumber % 360
+  const randomHue = hashedText % 360
   const randomSaturation =
-    (randomNumber % (maxSaturation - minSaturation)) + minSaturation
+    (hashedText % (maxSaturation - minSaturation)) + minSaturation
   const randomLightness =
-    (randomNumber % (maxLightness - minLightness)) + minLightness
+    (hashedText % (maxLightness - minLightness)) + minLightness
 
   return `hsl(${randomHue}deg ${randomSaturation}% ${randomLightness}%)`
+}
+
+function hash(str: string): number {
+  let hash = 0
+  for (let char of str) {
+    hash = (hash << 5) + char.charCodeAt(0)
+    hash |= 0
+  }
+
+  return hash
 }
